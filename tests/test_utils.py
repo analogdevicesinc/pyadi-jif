0262f3ba8c0ae874a247e5a5bf797ac5d01b2762
Travis F. Collins <travis.collins@analog.com>
2021-12-17 15:44:56 -0800

    Fix broken tests
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -23,6 +23,7 @@ import adijif as jif
 
 def test_generate_max_rates_fpga_utility():
     conv = jif.ad9081_rx()
+    conv.decimation = 1
 
     # Determine max data rate from FPGA
     fpga = jif.xilinx()
@@ -39,125 +40,132 @@ def test_generate_max_rates_fpga_utility():
             "bit_clock": 10000000000.0,
             "L": 8,
             "M": 1,
-            "quick_configuration_mode": "963",
+            "quick_configuration_mode": "19.01",
         },
         {
             "sample_clock": 3333333333.3333335,
             "bit_clock": 12500000000.0,
             "L": 8,
             "M": 2,
-            "quick_configuration_mode": "1263",
+            "quick_configuration_mode": "28.0",
         },
         {
             "sample_clock": 625000000.0,
             "bit_clock": 12500000000.0,
             "L": 3,
             "M": 3,
-            "quick_configuration_mode": "859",
+            "quick_configuration_mode": "9.01",
         },
         {
             "sample_clock": 1666666666.6666667,
             "bit_clock": 12500000000.0,
             "L": 8,
             "M": 4,
-            "quick_configuration_mode": "1261",
+            "quick_configuration_mode": "27.0",
         },
         {
             "sample_clock": 625000000.0,
             "bit_clock": 12500000000.0,
             "L": 6,
             "M": 6,
-            "quick_configuration_mode": "919",
+            "quick_configuration_mode": "15.01",
         },
         {
             "sample_clock": 833333333.3333334,
             "bit_clock": 12500000000.0,
             "L": 8,
             "M": 8,
-            "quick_configuration_mode": "1259",
+            "quick_configuration_mode": "26.0",
         },
         {
             "sample_clock": 312500000.0,
             "bit_clock": 12500000000.0,
             "L": 6,
             "M": 12,
-            "quick_configuration_mode": "206",
+            "quick_configuration_mode": "15.1",
         },
         {
             "sample_clock": 312500000.0,
             "bit_clock": 12500000000.0,
             "L": 8,
             "M": 16,
-            "quick_configuration_mode": "234",
+            "quick_configuration_mode": "17.0",
         },
     ]
-    print(results)
-    assert results == ref
+    assert len(results) == len(ref)
+    for result in results:
+        # print(result)
+        assert result in ref
 
 
 def test_generate_max_rates_utility():
     conv = jif.ad9081_rx()
+    conv.decimation = 1
 
     results = jif.utils.get_max_sample_rates(conv)
 
     ref = [
         {
-            "sample_clock": 4000000000.0,
-            "bit_clock": 20000000000.0,
-            "L": 4,
+            "L": 8,
             "M": 1,
-            "quick_configuration_mode": "913",
+            "bit_clock": 10000000000.0,
+            "quick_configuration_mode": "19.01",
+            "sample_clock": 4000000000.0,
         },
         {
-            "sample_clock": 4000000000.0,
-            "bit_clock": 20000000000.0,
             "L": 8,
             "M": 2,
-            "quick_configuration_mode": "262",
+            "bit_clock": 15000000000.0,
+            "quick_configuration_mode": "28.0",
+            "sample_clock": 4000000000.0,
         },
         {
-            "sample_clock": 1237500000.0,
-            "bit_clock": 24750000000.0,
             "L": 3,
             "M": 3,
-            "quick_configuration_mode": "859",
+            "bit_clock": 15500000000.0,
+            "quick_configuration_mode": "9.01",
+            "sample_clock": 775000000.0,
         },
         {
-            "sample_clock": 3300000000.0,
-            "bit_clock": 24750000000.0,
             "L": 8,
             "M": 4,
-            "quick_configuration_mode": "1261",
+            "bit_clock": 24750000000.0,
+            "quick_configuration_mode": "27.0",
+            "sample_clock": 3300000000.0,
         },
         {
-            "sample_clock": 1237500000.0,
-            "bit_clock": 24750000000.0,
             "L": 6,
             "M": 6,
-            "quick_configuration_mode": "919",
+            "bit_clock": 15500000000.0,
+            "quick_configuration_mode": "15.01",
+            "sample_clock": 775000000.0,
         },
         {
-            "sample_clock": 1650000000.0,
-            "bit_clock": 24750000000.0,
             "L": 8,
             "M": 8,
-            "quick_configuration_mode": "1259",
+            "bit_clock": 24750000000.0,
+            "quick_configuration_mode": "26.0",
+            "sample_clock": 1650000000.0,
         },
         {
-            "sample_clock": 618750000.0,
-            "bit_clock": 24750000000.0,
             "L": 6,
             "M": 12,
-            "quick_configuration_mode": "206",
+            "bit_clock": 15500000000.0,
+            "quick_configuration_mode": "15.1",
+            "sample_clock": 387500000.0,
         },
         {
-            "sample_clock": 618750000.0,
-            "bit_clock": 24750000000.0,
             "L": 8,
             "M": 16,
-            "quick_configuration_mode": "234",
+            "bit_clock": 15500000000.0,
+            "quick_configuration_mode": "17.0",
+            "sample_clock": 387500000.0,
         },
     ]
 
-    print(results)
-    # assert results == ref
+    pprint.pprint(results)
+
+    assert len(results) == len(ref)
+    for result in results:
+        print(result)
+        assert result in ref
