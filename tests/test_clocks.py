2003edaee60f3437b25dabfc46f96448776b34cc
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2022-05-09 11:37:34 -0400

    test_clocks: added a test to validate given r2
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

--- tests/test_clocks.py
+++ tests/test_clocks.py
@@ -92,7 +92,7 @@ def test_ad9545_fail_no_solver():
         clk.solve()
 
 
-def test_ad9523_1_daq2_validate():
+def test_ad9523_1_daq2_validate1():
 
     vcxo = 125000000
     n2 = 24
@@ -120,6 +120,34 @@ def test_ad9523_1_daq2_validate():
     assert o["n2"] == n2
     assert o["n2"] in clk.n2_available
     assert o["r2"] == 1
+    assert o["r2"] in clk.r2_available
+
+def test_ad9523_1_daq2_validate2():
+
+    vcxo = 125000000
+    
+    clk = adijif.ad9523_1()
+
+    # Check config valid
+    clk.r2 = 3
+    clk.use_vcxo_double = False
+
+    output_clocks = [1e9, 500e6, 7.8125e6]
+    clock_names = ["ADC", "FPGA", "SYSREF"]
+
+    clk.set_requested_clocks(vcxo, output_clocks, clock_names)
+
+    clk.solve()
+
+    o = clk.get_config()
+
+    # print(o)
+    
+    assert sorted(o["out_dividers"]) == [1, 2, 128]
+    assert o["m1"] == 3
+    assert o["m1"] in clk.m1_available
+    assert o["n2"] == 72
+    assert o["n2"] in clk.n2_available
     assert o["r2"] in clk.r2_available
 
 
