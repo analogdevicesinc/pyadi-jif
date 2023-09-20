e1065d8c9d29c29562567b83e16da44618fee61d
Liam Beguin <liambeguin@gmail.com>
2023-09-20 17:33:06 -0400

    tests: clocks: factorize ad9523_1_daq2_validate test case
    Use fixtures to drop duplicated code.
    
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- tests/test_clocks.py
+++ tests/test_clocks.py
@@ -92,12 +92,13 @@ def test_ad9545_fail_no_solver():
         clk.solve()
 
 
-def test_ad9523_1_daq2_validate():
+@pytest.mark.parametrize("solver", ["gekko", "CPLEX"])
+def test_ad9523_1_daq2_validate(solver):
 
     vcxo = 125000000
     n2 = 24
 
-    clk = adijif.ad9523_1()
+    clk = adijif.ad9523_1(solver=solver)
 
     # Check config valid
     clk.n2 = n2
@@ -111,40 +112,6 @@ def test_ad9523_1_daq2_validate():
     clk.solve()
 
     o = clk.get_config()
-
-    # print(o)
-
-    assert sorted(o["out_dividers"]) == [1, 2, 128]
-    assert o["m1"] == 3
-    assert o["m1"] in clk.m1_available
-    assert o["n2"] == n2
-    assert o["n2"] in clk.n2_available
-    assert o["r2"] == 1
-    assert o["r2"] in clk.r2_available
-
-
-def test_ad9523_1_daq2_cplex_validate():
-
-    vcxo = 125000000
-    n2 = 24
-
-    clk = adijif.ad9523_1(solver="CPLEX")
-    # clk = adijif.ad9523_1()
-
-    # Check config valid
-    clk.n2 = n2
-    clk.use_vcxo_double = False
-
-    output_clocks = [1e9, 500e6, 7.8125e6]
-    clock_names = ["ADC", "FPGA", "SYSREF"]
-
-    clk.set_requested_clocks(vcxo, output_clocks, clock_names)
-
-    clk.solve()
-
-    o = clk.get_config()
-
-    pprint.pprint(o)
 
     assert sorted(o["out_dividers"]) == [1, 2, 128]
     assert o["m1"] == 3
