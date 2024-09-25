a6903c28abb11c82021156284037e9a680a8645f
Travis F. Collins <travis.collins@analog.com>
2024-09-25 16:29:25 -0600

    Remove use of poetry
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- gui/gen_ui2py.sh
+++
@@ -1,3 +0,0 @@
-pyuic5 jif_gui/config.ui -o config.py
-pyuic5 jif_gui/main.ui -o main.py
-pyrcc5 jif_gui/resource.qrc -o resource_rc.py
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 # flake8: noqa
-def pytest_exception_interact(node, call, report):
-    excinfo = call.excinfo
-    if "script" in node.funcargs:
-        excinfo.traceback = excinfo.traceback.cut(path=node.funcargs["script"])
-    report.longrepr = node.repr_failure(excinfo)
+
+## Broken in 3.8+
+# def pytest_exception_interact(node, call, report):
+#     excinfo = call.excinfo
+#     if "script" in node.funcargs:
+#         excinfo.traceback = excinfo.traceback.cut(path=node.funcargs["script"])
+#     report.longrepr = node.repr_failure(excinfo)
