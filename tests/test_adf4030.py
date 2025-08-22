d9ced8305e97bdc60d4b4293bb7d0d32ffd0a268
Travis F. Collins <travis.collins@analog.com>
2025-08-22 09:55:27 -0600

    Add test for ADF4030 external clock chaining
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- tests/test_adf4030.py
+++ tests/test_adf4030.py
@@ -80,3 +80,66 @@ def test_adf4030_system():
 
     pprint.pprint(cfg)
     print(sys.converter.dac.converter_clock)
+
+
+def test_adf4030_chained_clocks():
+    import adijif
+
+    L = 2  # max_available_lanes_per_ad9084_side
+    M = 4  # Converters used to AD9084 side
+    Np = 16  # Bits per sample
+
+    vcxo = int(400e6)
+    cddc_dec = 4
+    fddc_dec = 8
+    converter_rate = int(12.8e9)
+
+    sys = adijif.system("ad9084_rx", "ltc6952", "xilinx", vcxo, solver="CPLEX")
+
+    sys.fpga.setup_by_dev_kit_name("vcu118")
+    sys.converter.sample_clock = converter_rate / (cddc_dec * fddc_dec)
+    sys.converter.datapath.cddc_decimations = [cddc_dec] * 4
+    sys.converter.datapath.fddc_decimations = [fddc_dec] * 8
+    sys.converter.datapath.fddc_enabled = [True] * 8
+
+    sys.converter.clocking_option = "direct"
+    sys.add_pll_inline("adf4382", vcxo, sys.converter)
+    # IMPLEMENTATION HERE
+    sys.add_pll_sysref("adf4030", sys.clock, sys.converter, sys.fpga)
+
+    sys.clock.minimize_feedback_dividers = False
+
+    mode_rx = adijif.utils.get_jesd_mode_from_params(
+        sys.converter, M=M, L=L, Np=Np, jesd_class="jesd204c"
+    )
+    print(f"RX JESD Mode: {mode_rx}")
+    assert mode_rx
+    mode_rx = mode_rx[0]["mode"]
+
+    sys.converter.set_quick_configuration_mode(mode_rx, "jesd204c")
+
+    print(f"Lane rate: {sys.converter.bit_clock/1e9} Gbps")
+    print(f"Needed Core clock: {sys.converter.bit_clock/66} MHz")
+
+    assert sys.converter.bit_clock == int(13.2e9)
+
+    cfg = sys.solve()
+
+    pprint.pprint(cfg)
+    # 'clock_ext_pll_sysref_adf4030': {'n': 155,
+    #                               'out_dividers': [4030],
+    #                               'output_clocks': {'AD9084_RX_sysref': {'divider': 4030,
+    #                                                                      'rate': 591715.976331361}},
+    #                               'r': 13,
+    #                               'vco': 2384615384.6153846},
+
+    # Verify sysref is within LMFC
+    mfc = sys.converter.multiframe_clock
+    estimate = cfg["clock_ext_pll_sysref_adf4030"]["output_clocks"]["AD9084_RX_sysref"][
+        "rate"
+    ]
+    for lmfc_div in range(1, 21):
+        sysref_rate = mfc / lmfc_div
+        if sysref_rate == estimate:
+            print(f"Found sysref match: {sysref_rate} with divisor {lmfc_div}")
+            return
